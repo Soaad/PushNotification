03017836e2c365acd64e045c50250390630a9963
Soaad <Soaad@users.noreply.github.com>
2021-06-10 23:23:20 +0200

    fix

--- push.js
+++ push.js
@@ -5,4 +5,8 @@ let vapIdKeys={
   }//push.generateVAPIDKeys();
 console.log(vapIdKeys)
 
-push.setVapidDetails("soaadabdelaziz@gmail.com",vapIdKeys.publicKey,vapIdKeys.privateKey)
+push.setVapidDetails("Test",vapIdKeys.publicKey,vapIdKeys.privateKey)
+
+let sub={"endpoint":"https://fcm.googleapis.com/fcm/send/exFN9JnqpDg:APA91bEduL_vRbp-nuIoI_RBU-gVHBsrADRic8fle-s1DyMQdjbLdio3l3JsAweuNhwAcXVEJmWSAWIUU7O9R8FFFNFp-AUc58CdLu_28xQd6z0hMGu894RNuBwpBVhvHMcHBZhEk9QB","expirationTime":null,"keys":{"p256dh":"BF9AmQhnsbLBk9XGAePW6dTmCBzsq8dR2UmttR8_rPG1VvxVVF-igE3TMwHpZxkR0VK5phYCQ0m-hc0Tl1oj7K8","auth":"1fPlR51xQiVelKgVnHyO2g"}}
+
+push.sendNotification(sub,'Test Message')
